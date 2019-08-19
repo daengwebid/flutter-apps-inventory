99cbdd0df7afc084d7b3925a4dcd95cd7782c9d5
Anugrah Sandi <makassarnetwork06@gmail.com>
2019-08-19 18:29:17 +0700

    Belajar Flutter Basic #8: State Management & Http Request

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Todo LIst",
+      theme: ThemeData.dark(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("TodoList Redux"),
+      ),
+      body: Container(),
+    );
+  }
+}
